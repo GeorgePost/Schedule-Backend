4e473f35d3d37da1aaef19e3304756949bd68035
GeorgePost <154926237+GeorgePost@users.noreply.github.com>
2024-09-02 20:39:46 -0400

    fixed some things for deployment

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express= require("express")
 const app = express();
+const helmet = require("helmet");
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
-app.use("/api/users", require("./routes/api/users"))
+app.use(express.urlencoded({extended:false}));
+app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+      },
+    }),
+  );
+app.use("/api/users", require("./routes/api/users"));
 app.listen(3000, ()=>{
     console.log("Server started listening on port 3000");
 });
