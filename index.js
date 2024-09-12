d9b7436054ba7fa149153cf29aa87875e34f3187
GeorgePost <154926237+GeorgePost@users.noreply.github.com>
2024-09-12 19:13:11 -0400

    added port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express= require("express")
 const app = express();
+const port = process.env.PORT || 4000;
 app.configure(function(){
   app.use(express.json());
   app.use(express.urlencoded({extended:false}));
@@ -14,6 +15,6 @@ app.configure(function(){
   })
   app.use("/api/users", require("./routes/api/users"));
 })
-app.listen(3000, ()=>{
-    console.log("Server started listening on port 3000");
+app.listen(port, ()=>{
+    console.log(`Server started listening on port ${port}`);
 });
