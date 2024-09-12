4c264a18624a6e4c92de2b020de4e340c7b8edcd
GeorgePost <154926237+GeorgePost@users.noreply.github.com>
2024-09-12 19:00:26 -0400

    app.configure()

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 const express= require("express")
-const app = express();
+const app = express.createServer();
 const helmet = require("helmet");
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-app.use(
-    helmet.contentSecurityPolicy({
-      directives: {
-        "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
-      },
-    }),
-  );
-app.use("/api/users", require("./routes/api/users"));
-app.use(function (req,res,next){
-  res.header("Access-Control-Allow-origin", "http://localhost:3000");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
+app.configure(function(){
+  app.use(express.json());
+  app.use(express.urlencoded({extended:false}));
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(function (req,res,next){
+    res.header("Access-Control-Allow-origin", "http://localhost:3000");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  })
+  app.use("/api/users", require("./routes/api/users"));
 })
 app.listen(3000, ()=>{
     console.log("Server started listening on port 3000");
