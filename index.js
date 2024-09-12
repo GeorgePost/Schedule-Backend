edf3e0e639b05b1f91f00c0fc4f95df5c3aead4c
GeorgePost <154926237+GeorgePost@users.noreply.github.com>
2024-09-12 18:47:59 -0400

    more headers

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ app.use(
     }),
   );
 app.use("/api/users", require("./routes/api/users"));
-app.use((req,res,next)=>{
+app.use(function (req,res,next){
   res.header("Access-Control-Allow-origin", "http://localhost:3000");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 })
 app.listen(3000, ()=>{
