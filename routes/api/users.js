a7a4814264f85bfb1f0ababbf384e87bbfcb343c
GeorgePost <154926237+GeorgePost@users.noreply.github.com>
2024-09-12 18:53:41 -0400

    added to the router

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,11 @@ const express= require("express");
 const router= express.Router();
 let users= require("../../Users");
 const { v4: uuidv4 } = require("uuid");
+router.options("/",  function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 router.get("/",(req,res)=>{
     res.json(users);
 })
