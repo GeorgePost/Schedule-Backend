db85950bdaa4cd1512a5266e15f88b8b084e4cb5
GeorgePost <154926237+GeorgePost@users.noreply.github.com>
2024-09-12 21:20:33 -0400

    app.configure is not a function?

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const express= require("express")
 const app = express();
 const port = process.env.PORT || 4000;
-app.configure(function(){
-  app.use(express.json());
-  app.use(express.urlencoded({extended:false}));
-  app.use(express.methodOverride());
-  app.use(express.bodyParser());
-  app.use(function (req,res,next){
-    res.header("Access-Control-Allow-origin", "http://localhost:3000");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-  })
-  app.use("/api/users", require("./routes/api/users"));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.methodOverride());
+app.use(express.bodyParser());
+app.use(function (req,res,next){
+  res.header("Access-Control-Allow-origin", "http://localhost:3000");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
 })
+app.use("/api/users", require("./routes/api/users"));
 app.listen(port, ()=>{
     console.log(`Server started listening on port ${port}`);
 });
