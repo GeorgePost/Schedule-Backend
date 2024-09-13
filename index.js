4f2246d03f1761579a9363977d766ecdce007859
GeorgePost <154926237+GeorgePost@users.noreply.github.com>
2024-09-12 22:10:01 -0400

    changed origins to work for production and testing

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const app = express();
 const port = process.env.PORT || 4000;
 const cors = require('cors');
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:3000",
-  headers: ["Content-Type"],
-  credentials: true,
-}));
+var allowlist = ['http://localhost:3000/Schedule-FrontEnd', 'https://georgepost.github.io/Schedule-FrontEnd/']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+app.use(cors(corsOptionsDelegate));
 app.use("/api/users", require("./routes/api/users"));
 app.options('*', cors());
 app.listen(port, ()=>{
